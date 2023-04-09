702ff1be65639ae2517bb71521487571b7cdc99c
Daniel Lam <Dann.lam@gmail.com>
2023-04-09 15:43:15 -0700

    API call to grab weather based on lat lons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,70 @@
-// //This is bad. Will be replaced.
+// //This is bad. I know. Will be replaced.
 temp_apikey = "9ce2af1a4de92e18eddd61ab40ca5e62";
 // //DOM elements interacted with.
 let searchQuery = document.getElementById("location-search-query");
 let searchBtn = document.getElementById("location-search-btn");
 
 
+//Take API data and display it.
+let buildForecast = (name, country, state, wind, humidity, temp, icon) => {
+        // http://openweathermap.org/img/wn/${icon}@2x.png
+}
+//Fetch the weather at location.
+let getWeather = (name, country, lat, lon, state) => {
+    //units set to imperial why? Because.
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${temp_apikey}`, {
+        //Future fetch options here if need be.
+        //Also needs error handlers.
+    })
+    .then(function (response){
+        //parse response
+        return response.json();
+    })
+    .then(function (data){
+        //Get relevent data
+        let icon = data.weather[0].icon;
+        let wind = data.wind.speed;
+        let humidity = data.main.humidity;
+        let temp = data.main.temp;
+        return buildForecast(name, country, state, wind, humidity, temp, icon);
+    })
 
-
-let feedMe = (name, country, lat, lon, state) => {
-    console.log(name, country, lat, lon, state)
 }
 // //GOAL: Take in a input search and apply it to a dropdown menu
 // //With our search results.
 // //When I click on the search button...
 // Right now we're grabbing the first result. We should modify this in the future to include a dropdwon menu of our 5 results so that someone could choose.
 let getCityCoords = (event) => {
-    console.log("Hi");
+    //Prevent default behavior of button clicked.
     event.preventDefault();
-    fetch('http://api.openweathermap.org/geo/1.0/direct?q=Providence&limit=5&appid=9ce2af1a4de92e18eddd61ab40ca5e62', {
+    //Get text from queryBox and trim it.
+    let searchQueryText = searchQuery.value.trim();
+    //Ensure we have something
+    if(searchQueryText){
+        //fetch city
+        fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${searchQueryText}&limit=5&appid=${temp_apikey}`, {
 
         //Fetch options here if need be.
+        //We should probably add in some error handlers later.
       })
         .then(function (response) {
-            //We write the remainder of the fetch() request, as follows:
+          //parse response to useful
           return response.json();
         })
+        //Grab the data that we want.
         .then(function (data) {
-          console.log(data);
           let cityName = data[0].name;
           let cityCountry = data[0].country;
           let cityLat = data[0].lat
           let cityLon = data[0].lon
           let cityState = data[0].state
-          console.log(cityName, cityCountry, cityLat, cityLon, cityState)
-          return feedMe(cityName, cityCountry, cityLat, cityLon, cityState)
+          return getWeather(cityName, cityCountry, cityLat, cityLon, cityState)
         });
+    } else {
+        //Condition for empty input box.
+        console.log("Please input something in the text box!")
+    }
+
 
 }
 
